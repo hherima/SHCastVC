e631748ef6373fd51c5bc00100d81ba08454e1eb
guoxiaodong <xiaodongguo@sohu-inc.com>
2021-03-08 13:29:19 +0800

    1.0.1

---
+++ Classes/SHCastTest.h
@@ -0,0 +1,16 @@
+//
+//  SHCastTest.h
+//  SHCastTV
+//
+//  Created by guoxiaodong on 2021/3/8.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SHCastTest : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Classes/SHCastTest.m
@@ -0,0 +1,12 @@
+//
+//  SHCastTest.m
+//  SHCastTV
+//
+//  Created by guoxiaodong on 2021/3/8.
+//
+
+#import "SHCastTest.h"
+
+@implementation SHCastTest
+
+@end
